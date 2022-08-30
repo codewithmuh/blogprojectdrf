54b913d2d6e3a0f31c635464e5f0d79fb1440aea
Muhammad Rashid <rashiddaha80@gmail.com>
2022-08-31 02:08:30 +0500

    added host

--- blog/settings.py
+++ blog/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3_+25_zerd=%j4c%c%cg00k!gbnr@(ho09^9nlc+_!wepee@&('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
