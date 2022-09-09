1fe3b84e2d131b2d895e032bf08aad8ab6ecec16
Muhammad Rashid <rashiddaha80@gmail.com>
2022-09-09 12:54:02 +0500

    swaggers Added

--- blog/settings.py
+++ blog/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'Account',
     'blogapp',  
+    'drf_yasg',
 ]
 
 MIDDLEWARE = [
--- blog/urls.py
+++ blog/urls.py
@@ -17,10 +17,23 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings # new
 from  django.conf.urls.static import static #new
+from drf_yasg import openapi #new foe swagger
+from drf_yasg.views import get_schema_view as swagger_get_schema_view #new foe swagger
+
+
+schema_view = swagger_get_schema_view(
+    openapi.Info(
+        title="Blogs API",
+        default_version='1.0.0',
+        description="API documentation of App",
+    ),
+    public=True,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('swagger/schema/', schema_view.with_ui('swagger', cache_timeout=0), name="swagger-schema"),
 
 ]
 if settings.DEBUG:
