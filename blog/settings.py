7a214dd2a7d99dde15d3ecd1326b016459587492
M. Rashid <51082957+rashiddaha@users.noreply.github.com>
2022-10-07 11:31:43 +0500

    Update settings.py

--- blog/settings.py
+++ blog/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-3_+25_zerd=%j4c%c%cg00k!gbnr@(ho09^9nlc+_!wepee@&(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['35.161.198.10']
 
 
 # Application definition
