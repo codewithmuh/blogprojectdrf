b23bc4e3d92e8a0d18a3c6991e328f44f03eda0c
Muhammad Rashid <rashiddaha80@gmail.com>
2023-01-25 20:31:03 +0500

    update

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -7,9 +7,6 @@ sudo rm -f /etc/nginx/sites-enabled/default
 
 sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/blog
 sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-sudo systemctl start nginx
-sudo systemctl reload nginx
-sudo systemctl restart nginx
 sudo nginx -t
 sudo systemctl restart nginx
 sudo ufw delete allow 8000
