7fda6959594fc25e93529a011991e24761eefda5
Muhammad Rashid <rashiddaha80@gmail.com>
2023-01-25 18:46:19 +0500

    update

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blogprojectdrf/main/settings.py
+sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
 python  manage.py makemigrations
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
