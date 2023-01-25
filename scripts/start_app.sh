4bac8d07f18c98c2d8a80dc8af2b7554e97140c9
Muhammad Rashid <rashiddaha80@gmail.com>
2023-01-25 19:09:27 +0500

    update

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -3,3 +3,7 @@
 sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
+sudo service gunicorn restart
+sudo service nginx restart
+
+
