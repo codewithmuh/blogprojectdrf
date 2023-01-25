f8c968a96c2eda597d563ddca56a93fae62ef905
Muhammad Rashid <rashiddaha80@gmail.com>
2023-01-25 18:57:01 +0500

    update

--- scripts/gunicorn.sh
+++ scripts/gunicorn.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash
-
+sudo cp /home/ubuntu/blogprojectdrf/gunicorn/gunicorn.socket  /etc/systemd/system/gunicorn.socket
 sudo cp /home/ubuntu/blogprojectdrf/gunicorn/gunicorn.service  /etc/systemd/system/gunicorn.service
 
 sudo systemctl start gunicorn
