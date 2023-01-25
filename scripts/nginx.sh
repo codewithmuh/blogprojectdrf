d5800c7305593e80f6d3d9c52d6dda5d9651ecb7
Muhammad Rashid <rashiddaha80@gmail.com>
2023-01-25 19:36:38 +0500

    update

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,8 +4,6 @@ sudo systemctl daemon-reload
 systemctl restart gunicorn.service
 sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/blog
 sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
-sudo fuser -k 80/tcp
-sudo fuser -k 443/tcp
 sudo service nginx restart
 sudo nginx -t
 sudo systemctl restart nginx
--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -5,6 +5,7 @@ sed -i 's/\[]/\["54.144.250.113"]/' /home/ubuntu/blogprojectdrf/blog/settings.py
 sudo service gunicorn restart
 sudo fuser -k 80/tcp
 sudo fuser -k 443/tcp
+sudo systemctl restart nginx
 sudo service nginx restart
 
 
