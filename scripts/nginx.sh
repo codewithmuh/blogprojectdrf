1bd6a0ff1ca1c0e4c5812d60e89eb54e4aecc8d3
Muhammad Rashid <rashiddaha80@gmail.com>
2023-01-25 19:14:01 +0500

    update

--- scripts/nginx.sh
+++ scripts/nginx.sh
@@ -4,6 +4,8 @@ sudo systemctl daemon-reload
 systemctl restart gunicorn.service
 sudo cp /home/ubuntu/blogprojectdrf/nginx/nginx.conf /etc/nginx/sites-available/blog
 sudo ln -s /etc/nginx/sites-available/blog /etc/nginx/sites-enabled
+sudo fuser -k 80/tcp
+sudo fuser -k 443/tcp
 sudo nginx -t
 sudo systemctl restart nginx
 sudo ufw delete allow 8000
